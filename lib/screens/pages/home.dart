7d5c026bbc4725e1002ee81a853368c6becbc8e8
TeamFunction <project.spp.rpl@gmail.com>
2022-10-31 14:53:43 +0700

    memperbarui home

--- lib/screens/pages/home.dart
+++ lib/screens/pages/home.dart
@@ -1,44 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_spp/conf.dart';
+import 'package:project_spp/models/intro_model.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-  
+
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(
+              bgRTE,
+              width: MediaQuery.of(context).size.width * 0.65,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.topLeft,
+            child: SvgPicture.asset(bgLTE,
+                width: MediaQuery.of(context).size.width * 0.5),
+          ),
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    "SELAMAT DATANG DI APLIKASI PEMBAYARAN SPP",
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 28,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.5),
+                            offset: Offset(2, 2),
+                            blurRadius: 15,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    decoration: BoxDecoration(
+                      color: Colors.red.withOpacity(0.2),
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(20),
+                      // backgroundBlendMode: BlendMode.src,
+                      boxShadow: [
+                        BoxShadow(
+                          blurStyle: BlurStyle.outer,
+                          color: Colors.grey.withOpacity(0.8),
+                          blurRadius: 20,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-        _counter++;
-    });
+class InvertedClipper extends CustomClipper<Path> {
+  InvertedClipper();
+
+  @override
+  Path getClip(Size size) {
+    return Path.combine(
+      PathOperation.difference,
+      Path()..addRect(Rect.fromLTWH(1, 1, size.width + 10, size.height + 10)),
+      Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height)),
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-       
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
